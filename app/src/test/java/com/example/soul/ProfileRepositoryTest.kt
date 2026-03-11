4c15f60ecfbf3e5fcc30154e79660ca8f651612a
aman gond <agjkfg2234@gmail.com>
2026-03-11 15:37:44 +0530

    sdf

--- app/src/test/java/com/example/soul/ProfileRepositoryTest.kt
+++ app/src/test/java/com/example/soul/ProfileRepositoryTest.kt
@@ -22,13 +22,16 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.TestCoroutineScheduler
+import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
@@ -149,16 +152,50 @@ private val fakeData=UserData(name = "animora", branch ="CSIT", rollNo = "cs89",
         val userId = "id69"
         every { backendComplaint.uidFlow } returns flowOf(userId)
         coEvery { daoProfile.getUser(any()) } returns null
-        coEvery { backendProfile.userDataProfileFetch(any()) } returns UserProfileData.Error(Exception("time out"))
+        coEvery { backendProfile.userDataProfileFetch(any()) } returns UserProfileData.Error("not connect network")
 
         val result=  profileRepository.fetchProfileData()
-        assertEquals(UserProfileDataStateRepository.Error("time out"),result)
+        assertEquals(UserProfileDataStateRepository.Error("not connect network"),result)
         coVerify{ daoProfile.getUser(userId) }
         coVerify(exactly = 1) { backendProfile.userDataProfileFetch(userId) }
         coVerify(exactly = 0) { daoProfile.insertProfile(any()) }
     }
 
 
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun fetchProfileData_parallelCalls_shouldRemoteCalsOnce()= runTest {
+
+        val profileRepository=instance(testScheduler)
+
+        var isInserted=false
+        val userId = "id69"
+        every { backendComplaint.uidFlow } returns flowOf(userId)
+        coEvery { daoProfile.getUser(any()) } coAnswers {
+            if(isInserted){
+                ProfileRoom.ProfileEntity(uid = userId)
+            }
+            else{
+                null
+            }
+        }
+
+        coEvery { backendProfile.userDataProfileFetch(any()) } coAnswers {
+            isInserted=true
+            UserProfileData.Success(fakeData)
+        }
+
+        coEvery { daoProfile.insertProfile(any()) } just Runs
+
+        launch { profileRepository.fetchProfileData() }
+        launch { profileRepository.fetchProfileData() }
+
+        advanceUntilIdle()
+
+        coVerify(exactly = 1) { backendProfile.userDataProfileFetch(userId) }
+        coVerify(exactly = 1) { daoProfile.insertProfile(match { it.uid==userId }) }
+    }
 
 
 
@@ -219,7 +256,7 @@ private val fakeData=UserData(name = "animora", branch ="CSIT", rollNo = "cs89",
 
 
     @Test
-    fun observeInfo_whenRoomThrowError_shouldEmitsLoadingAndError()= runTest {
+    fun observeInfo_whenRoomThrowError_shouldEmitsLoadingAndError()= runTest {// inner room exception wrap so now the upper flow will not terminated
         val profileRepository=instance(testScheduler)
         val userId = "id69"
         every { backendComplaint.uidFlow } returns flowOf(userId)
@@ -228,7 +265,7 @@ private val fakeData=UserData(name = "animora", branch ="CSIT", rollNo = "cs89",
         }
 
         profileRepository.observeUserInfo().test {
-            assertTrue(awaitItem() is ProfileFetchRoom.Loading)
+            assertEquals(ProfileFetchRoom.Loading, awaitItem())
 
             val result =  awaitItem() as ProfileFetchRoom.Error
             assertEquals("DB error",result.message)
@@ -239,7 +276,7 @@ private val fakeData=UserData(name = "animora", branch ="CSIT", rollNo = "cs89",
 
     @Test
     fun observeInfo_whenSameDataEmitsTwice_shouldEmitsOnlyOnce()= runTest {
-        // This test protects the distinctUntilChanged() behaviour
+        // This test protects the distinctUntilChanged() behaviour because its stop emitting of same value more than one time
     // If distinctUntilChanged() is removed from the repository flow
     // duplicate Success emissions may occur when Room emits the same entity twice
     // This test ensures the repository emits Success only once
@@ -251,21 +288,47 @@ private val fakeData=UserData(name = "animora", branch ="CSIT", rollNo = "cs89",
 
         coEvery { daoProfile.observeUser(any()) } returns dbFlow
         profileRepository.observeUserInfo().test {
-
-            assertTrue(awaitItem() is ProfileFetchRoom.Loading)// awaitItem() its return the value from turbine queue
-
+            assertEquals(ProfileFetchRoom.Loading, awaitItem())// awaitItem() its return the value from turbine queue
             val  entity1=ProfileRoom.ProfileEntity(uid = userId)
             dbFlow.emit(entity1)
             assertTrue(awaitItem() is ProfileFetchRoom.Success)
-
-
             val  entity2=ProfileRoom.ProfileEntity(uid = userId)
-            dbFlow.emit(entity2)
-
+            dbFlow.emit(entity2)// its drop by the distinctUnitChanged() so not need awaitItem
             expectNoEvents()// its check is turbine queue isEmpty
+            cancelAndIgnoreRemainingEvents()
         }
         coVerify(exactly = 1){ daoProfile.observeUser(userId) }
     }
+
+
+
+    @Test
+    fun observerInfo_whenUpdateEmptyRoom_shouldEmitsLoadingEmptyThenSuccess()= runTest {
+        val profileRepository=instance(testScheduler)
+        val userId = "id69"
+        val dbFlow = MutableSharedFlow<ProfileRoom.ProfileEntity?>()
+        every { backendComplaint.uidFlow } returns flowOf(userId)
+        coEvery { daoProfile.observeUser(any()) } returns dbFlow
+        profileRepository.observeUserInfo().test {
+            assertEquals(ProfileFetchRoom.Loading, awaitItem())
+
+            dbFlow.emit(null)
+            assertEquals(ProfileFetchRoom.Empty, awaitItem())
+
+            val  entity1=ProfileRoom.ProfileEntity(uid = userId)
+            dbFlow.emit(entity1)
+            val result=awaitItem() as ProfileFetchRoom.Success
+            assertEquals(userId,result.data.uid)
+
+            expectNoEvents()
+            cancelAndIgnoreRemainingEvents()
+        }
+        coVerify(exactly = 1) { daoProfile.observeUser(uid = userId) }
+    }
+
+    // old cancel test write when use multiple account switching from the profile screen
+
+
 
 
 
